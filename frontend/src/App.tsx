ea845597eec58123520b94a7ede9756620516791
JoseMendesSilva <devops.josemendes@gmail.com>
2022-07-12 00:57:11 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import NotificationButton from './components/NotificationButton'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <h1>Olá Mundo!</h1>
+    <NotificationButton />
+    </>
   )
 }
 
